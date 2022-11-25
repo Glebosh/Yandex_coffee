879e92b24e3e4ed05b5592736756e1eeca61e405
Glebosh <glebosh@gmail.com>
2022-11-26 00:06:10 +0300

    error delet

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication
-from PyQt5.QtWidgets import QMainWindow QTableWidgetItem
+from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem
 
 
 class Main(QMainWindow):
